b0b96c2120ed386df07095ba8e6f8fa4ca1679be
lsh9084 <sanghun1101088@gmail.com>
2024-07-12 09:15:29 +0900

    0712

--- src/main/java/com/ssamz/web/user/LoginServlet.java
+++ src/main/java/com/ssamz/web/user/LoginServlet.java
@@ -1,17 +1,32 @@
 package com.ssamz.web.user;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet("/login.do")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     public LoginServlet() {
         System.out.println("===>LoginServlet 생성");
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	System.out.println("===> init() 호출");
+    }
+    
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    	System.out.println("===> service() 호출");
+    
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -22,6 +37,12 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		System.out.println("doPost방식의 요청 처리...");
+	}
+	   
+	@Override
+	public void destroy() {
+		System.out.println("destroy() 호출");
+	    
 	}
 
 }
